512ebf8e51ba1b9e1ebca003b71a74aedc5bb88c
Md Hasnath Karim <hasnath.karim@gmail.com>
2025-05-01 03:45:56 +0100

    flip-card

---
+++ README.md
@@ -0,0 +1,20 @@
+# 📚 Project Name
+
+Knowledge_BD
+
+
+> A flashcard quiz app built with Python and Tkinter to help users study Q&A from a CSV file.
+
+
+
+## 🚀 Features
+
+- Randomly displays questions from a CSV file
+- Flips to show the answer after a short delay
+- Clean UI with images and buttons
+- Option to mark questions as known or unknown
+
+---
+
+
+
---
+++ main.py
@@ -0,0 +1,72 @@
+from tkinter import *
+from PIL import Image, ImageTk
+import pandas as pd
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+current_card = {}
+to_learn={}
+
+
+try:
+    data = pd.read_csv("answer_to_learn.csv", usecols=["Question", "Answer"])
+except FileNotFoundError:
+    original_data = pd.read_csv("Q_A.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
+window = Tk()
+window.title("Knowledge-BD")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+front_bg_img = ImageTk.PhotoImage(Image.open("images/card_front.png"))  # Front
+back_bg_img = ImageTk.PhotoImage(Image.open("images/card_back.png"))   # Back
+
+canvas = Canvas(window, width=800, height=400, bg=BACKGROUND_COLOR, highlightthickness=0)
+card_bg = canvas.create_image(400, 200, image=front_bg_img)  # Centered
+card_question = canvas.create_text(400, 120, text="", font=("Ariel", 20, "italic"))
+card_answer = canvas.create_text(400, 250, text="", font=("Ariel", 20, "bold"))
+canvas.grid(row=0, column=0, columnspan=2)
+
+flip_timer=None
+
+
+def next_card():
+    global current_card, flip_timer
+    if flip_timer:
+        window.after_cancel(flip_timer)
+
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_bg, image=front_bg_img)
+    canvas.itemconfig(card_question, text="Question", fill="black")
+    canvas.itemconfig(card_answer, text=current_card["Question"], fill="black")
+
+    flip_timer=window.after(3000,flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_bg, image=back_bg_img)
+    canvas.itemconfig(card_question, text="Answer",fill="white")
+    canvas.itemconfig(card_answer, text=current_card["Answer"], fill="white")
+
+def is_known():
+    to_learn.remove(current_card)
+    data=pd.DataFrame(to_learn)
+    data.to_csv("answer_to_learn.csv", index=False)
+    next_card()
+
+
+cross_image = ImageTk.PhotoImage(Image.open("images/wrong.png"))
+unknown_button = Button(image=cross_image, highlightthickness=0, border=0, command=next_card)
+unknown_button.grid(row=1, column=0)
+
+check_image = ImageTk.PhotoImage(Image.open("images/right.png"))
+known_button = Button(image=check_image, highlightthickness=0, border=0, command=is_known)
+known_button.grid(row=1, column=1)
+
+
+next_card()
+window.mainloop()
